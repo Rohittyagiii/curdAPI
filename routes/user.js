5fe2cd04200632473824b915da9c5ace944ccdef
Rohittyagiii <tyagirohit698@gmail.com>
2026-04-01 22:51:38 +0530

    commit deleteAPIuser

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 const User = require("../models/User");
@@ -7,17 +7,44 @@ router.get("/health", (req, res) => {
   res.json({ status: "API is working" });
 });
 
-router.get("/users", async(req,res) => {
-    const users = await User.find()
-    res.json({status:"Get all Users"},users);
-})
-
-router.post("/users", async(req,res) => {
-  const user = new User(req.body)
-    const savedUser = await user.save();
-     res.status(201).json(savedUser);
+router.get("/users", async (req, res) => {
+  const users = await User.find();
+  let abc = {
+    msg: "Data found successfully",
+    status: true,
+    data: users,
+  };
+  res.json(abc);
 });
 
+router.post("/addusers", async (req, res) => {
+  const user = new User(req.body);
+  const savedUser = await user.save();
+  res.status(201).json(savedUser);
+});
 
+router.delete("/deleteByID", async (req, res) => {
+  const {deletedUserId} = req.body;
+   
+  if (!deletedUserId) {
+    return res.status(400).json({
+      msg: "UserId is not found in request payload",
+      myid:deletedUserId
+    });
+  }  
+  
+    const deletedUser = await User.findByIdAndDelete(deletedUserId);
+    if (!deletedUser) {
+      return res.status(404).json({
+        msg: "User not found",
+      });
+    }
+
+    res.json({
+       msg: "User deleted successfully",
+      user: deletedUser,
+    });
+  
+});
 
 module.exports = router;
